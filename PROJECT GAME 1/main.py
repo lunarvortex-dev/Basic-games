adae58be8c933444d0af06c614d5c9c689b1d73c
ashish-codes <singhashish24721@gmail.com>
2026-06-22 20:58:38 +0530

    Refactor rock-paper-scissors game logic

--- PROJECT GAME 1/main.py
+++ PROJECT GAME 1/main.py
@@ -7,23 +7,20 @@ Computer = random.choice([1,2,3])
 user = int(input("Enter your choice : "))
 youdict = {1:"Stone",2:"Paper",3:"Scissors"}
 
-if(user not in [1,2,3]):
+wins = {1: 3, 2: 1, 3: 2}
+
+if user not in [1, 2, 3]:
     print("Invalid input!")
 else:
+    Computer = random.choice([1, 2, 3])
+
     print(f"Computer chose {youdict[Computer]} and You chose {youdict[user]}")
 
-if(Computer == user):
-    print("Its a Draw!")
-elif(Computer == 1 and user == 2):
-    print("You win!")
-elif(Computer == 1 and user == 3): 
-    print("You lose!")
-elif(Computer == 2 and user == 1):
-    print("You lose!")
-elif(Computer == 2 and user == 3):
-    print("You win!")
-elif(Computer == 3 and user == 1):
-    print("You win!")
-elif(Computer == 3 and user == 2):
-    print("You lose!")
+    if user == Computer:
+        print("It's a Draw!")
 
+    elif wins[user] == Computer:
+        print("You win!")
+
+    else:
+        print("You lose!")
